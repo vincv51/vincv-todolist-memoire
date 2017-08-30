fb932651409a06011668cc952c615a05fd93c20b
Vincent VAIRAC <vincv51@yahoo.fr>
2017-08-29 19:18:17 -0500

    Création de ma todolist

--- app.js
+++ app.js
@@ -1,28 +1,63 @@
-/*eslint-env node*/
-
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
-
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
 var express = require('express');
+var session = require('cookie-session'); // Charge le middleware de sessions
+var bodyParser = require('body-parser'); // Charge le middleware de gestion des paramètres
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var Cloudant = require('cloudant');
 
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
 
-// create a new express server
 var app = express();
 
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+// Définition de l'hôte et du port
+var host        = process.env.VCAP_APP_HOST || process.env.HOST || 'localhost';
+var port        = process.env.VCAP_APP_PORT || process.env.PORT || 8080;
 
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
 
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+var cloudant = Cloudant({account:'4f231165-2664-4634-9018-4c8de17afed7-bluemix', password:'6846a4cd22e7999ca786921e7ac66cc86a34fe12ea8dd7d10b7ace0cf418f097'}, function(err, cloudant) {
+    if (err) {
+        return console.log('Failed to initialize Cloudant: ' + err.message);
+    }
+
+    var db = cloudant.db.use('todolist');
+
+    /* On utilise les sessions */
+    app.use(session({secret: 'todotopsecret'}))
+
+    /* On affiche la todolist et le formulaire */
+    .get('/todo', function(req, res) { 
+        db.list({include_docs: true}, function(err, body) {
+            if (!err) {
+                res.render('todo.ejs', {todolist: body.rows});
+            }
+        });
+    })
+
+    /* On ajoute un élément à la todolist */
+    .post('/todo/ajouter/', urlencodedParser, function(req, res) {
+        if (req.body.newtodo != '') {
+            var newdata = {'todo': req.body.newtodo};
+            db.insert(newdata, function(err, result) {
+                res.redirect('/todo');
+            });
+        }
+    })
+
+    /* Supprime un élément de la todolist */
+    .get('/todo/supprimer/:id', function(req, res) {
+        if (req.params.id != '') {
+            db.get(req.params.id, { revs_info: true }, function(err, body) {
+                if (!err) {
+                    db.destroy(req.params.id, body._rev, function(err, body) {
+                        res.redirect('/todo');
+                    });
+                }
+            });
+        }
+    })
+
+    /* On redirige vers la todolist si la page demandée n'est pas trouvée */
+    .use(function(req, res, next){
+        res.redirect('/todo');
+    })
+
+    .listen(port, host);
 });
